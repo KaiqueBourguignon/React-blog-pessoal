a1bebbbe7b334bc417da7dfd42971d2e050754a9
KaiqueBourguignon <kaiquegeneration@gmail.com>
2022-12-01 15:57:06 -0300

    feat: footes , nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
-import { About } from './paginas/About/About';
-import { Home } from './paginas/Home/Home';
+import Footer from './components/estaticos/footer/Footer';
+import Home from './paginas/home/Home';
+import './App.css';
+import Login from './paginas/login/Login';
+
 
 function App() {
   return (
     <Router>
       <Navbar />
-      
-
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/about' element={<About />} />
+
+        <Route path="/" element={<Home />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
       </Routes>
-
-      
-
+      <Footer />
     </Router>
-  
 
   );
 }
